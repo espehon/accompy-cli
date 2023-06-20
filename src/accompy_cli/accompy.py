7d71aa3d38b58edcccdfce3b376ee0692b73e57f
espehon <gregg.howlett@gmail.com>
2023-06-20 15:23:22 -0700

    added body

--- src/accompy_cli/accompy.py
+++ src/accompy_cli/accompy.py
@@ -6,7 +6,7 @@ import json
 from colorama import Fore, init
 
 
-# template for new entries. Not to be used for anything else: Not even as a header. This should be moved to json in ~/.config eventually.
+# template for new entries. Not to be used for anything else: Not even as a header unless used as a starting point. This should be moved to json in ~/.config eventually.
 template = ['title', 'start date', 'end date', 'situation', 'action', 'result']
 
 # storage_path = os.path.expanduser("~/.local/share/accompy/storage.json")
@@ -16,3 +16,26 @@ with open(storage_path, 'r') as f:
     data = json.load(f)
 
 print(data[1]['title'])
+
+
+# Build header
+header = template
+for entry in data:
+    for key in entry:
+        if key not in header:
+            header.append(key)
+
+# Build body
+body = []
+for index, entry in enumerate(data):
+    body.append([])
+    for column in header:
+        if column in entry:
+            body[index].append(entry[column])
+        else:
+            body[index].append('')
+
+
+
+
+
