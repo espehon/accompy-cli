7378214322f3fee6847fb110267b603a443b78da
espehon <gregg.howlett@gmail.com>
2023-06-19 18:35:30 -0700

    Added basic json IO

--- src/accompy_cli/accompy.py
+++ src/accompy_cli/accompy.py
@@ -1,2 +1,14 @@
 # Copyright (c) 2023, espehon
 # License: https://www.gnu.org/licenses/gpl-3.0.html
+
+import os
+import json
+
+
+# storage_path = os.path.expanduser("~/.local/share/accompy/storage.json")
+storage_path = os.path.expanduser("~/github/accompy/tests/base.json")
+
+with open(storage_path, 'r') as f:
+    data = json.load(f)
+
+print(data)
