c74928c49b81d192ca7d33f4ad182948c217fca3
espehon <gregg.howlett@gmail.com>
2023-07-02 12:50:06 -0700

    refactored code to function

--- src/accompy_cli/accompy.py
+++ src/accompy_cli/accompy.py
@@ -16,46 +16,43 @@ with open(storage_path, 'r') as f:
     data = json.load(f)
 
 
+def output_data_as_table(table_data):
+    # Build header
+    header = template
+    for entry in table_data:
+        for key in entry:
+            if key not in header:
+                header.append(key)
 
-# Build header
-header = template
-for entry in data:
-    for key in entry:
-        if key not in header:
-            header.append(key)
+    # Build body
+    body = []
+    for index, entry in enumerate(table_data):
+        body.append([])
+        for column in header:
+            if column in entry:
+                body[index].append(entry[column])
+            else:
+                body[index].append('')
 
-# Build body
-body = []
-for index, entry in enumerate(data):
-    body.append([])
-    for column in header:
-        if column in entry:
-            body[index].append(entry[column])
-        else:
-            body[index].append('')
+    # Calculate column widths
+    padding = 4
+    widths = []
+    for title in header:    # First, get widths of header elements
+        widths.append(len(title))
+    for row in body:    # Next, expand widths if any body elements are wider
+        for index, size in enumerate(widths):
+            if len(row[index]) > size:
+                widths[index] = len(row[index])
+    for index, size in enumerate(widths):   # Finally, add padding.
+            widths[index] = size + padding
 
-# Calculate column widths
-padding = 4
-widths = []
-for title in header:    # First, get widths of header elements
-    widths.append(len(title))
-for row in body:    # Next, expand widths if any body elements are wider
-    for index, size in enumerate(widths):
-        if len(row[index]) > size:
-            widths[index] = len(row[index])
-for index, size in enumerate(widths):   # Finally, add padding.
-        widths[index] = size + padding
-
-
-
-# Print table
-
-output = ''
-for index, column in enumerate(header):
-    output += column.ljust(widths[index])
-for row in body:
-    output += '\n'
-    for index, column in enumerate(row):
+    # Print table
+    output = ''
+    for index, column in enumerate(header):
         output += column.ljust(widths[index])
+    for row in body:
+        output += '\n'
+        for index, column in enumerate(row):
+            output += column.ljust(widths[index])
 
-print(output)
+    print(output)
